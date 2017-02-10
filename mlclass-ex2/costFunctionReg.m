370e1a255e10650af09c509cbb1b6d8aaa7337bf
ruichaowang <wrc582029763@gmail.com>
2017-02-09 19:57:56 -0500

    Update costFunctionReg.m
    The code before is wrong.

--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -21,19 +21,20 @@ grad = zeros(size(theta));
 z=X*theta;
 h=sigmoid(z);
 logisf=(-y)'*log(h)-(1-y)'*log(1-h);
-
+theta(1)=0; %set we should not regularize the parameter theta0
 J=((1/m).*sum(logisf))+(lambda/(2*m)).*sum(theta.^2);
-
-k=length(theta)-1;
-n=length(theta);
+grad=(1/m).*((X'*h-X'*y)+theta*lambda);
+%k=length(theta)-1;
+%n=length(theta);
 %grad(1)=1/m.*(X'(1)*h-X'(1)*y(1));
-grad(1)=1/m.*(sum(X'(1,:)*h-X'(1,:)*y));
+%grad(1)=1/m.*(sum(X'(1,:)*h-X'(1,:)*y));
 %grad(1)=1/m.*(X'*h-X'*y);
 %grad=(1/m).*((X'*h-X'*y)+theta*lambda);  % This gives correct answer but ...
 
-for j=2:n
-	grad(j)=(1/m).*(sum(X'(j,:)*h-X'(j,:)*y)+lambda.*theta(j,1));
-end
+%for j=2:n
+%	grad(j)=(1/m).*(sum(X'(j,:)*h-X'(j,:)*y)+lambda.*theta(j,1));
+%end
+
 
 % =============================================================
 
